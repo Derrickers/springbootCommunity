4d4cb3459f78613020eb1c70bfd282269c537af1
Derrickers <656018919@qq.com>
2020-02-09 13:12:54 +0800

    question modify block

--- README.md
+++ README.md
@@ -12,12 +12,19 @@
 [Bootstrap](https://v3.bootcss.com/getting-started/)
 
 [Github OAuth 文档](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+[Thymeleaf](https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html)
+
 ## 工具
 [Git](https://git-scm.com/download)
 
 [Visual-Paradigm](https://www.visual-paradigm.com)
 
 [FlyWay](https://flywaydb.org)
+
+[Lombok](https://www.projectlombok.org)
+
+[]()
 
 ## 脚本
 ```sql
--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,21 +1,12 @@
 package com.example.community.controller;
 
 import com.example.community.dto.PaginationDTO;
-import com.example.community.dto.QuestionDTO;
-import com.example.community.mapper.QuestionMapper;
-import com.example.community.mapper.UserMapper;
-import com.example.community.model.Question;
-import com.example.community.model.User;
 import com.example.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * Created by Derricker on 2020/2/2
@@ -24,29 +15,12 @@ import java.util.List;
 public class IndexController {
 
     @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
-                        @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "5") Integer size){
-        //从浏览器请求cookie
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null)
-            for(Cookie cookie : cookies){
-                if(cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if(user != null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                    break;
-                }
-            }
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size){
         //跳转之前显示
         PaginationDTO paginationDTO = questionService.list(page,size);
         model.addAttribute("pagination",paginationDTO);
